9a759744139edfdbf4d459cdbf1d0a951fa4f420
nikhilsondur <77484064+nikhilsondur@users.noreply.github.com>
2021-01-29 17:03:13 +0530

    Update stats.c
    updating the compute statistics function with required code to find and return structure containing min max and avg.

--- stats.c
+++ stats.c
@@ -5,6 +5,35 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    float sum;
+    int count = 0;
+    
+    while(count < setlength) //using while loop to find the average.
+    {
+     sum+=numberset[count];   
+     count+=1;
+    }
+    
+    s.average = sum/setlength; //Calculating the average once the sum is found.
+    
+    int location = 0;  //initialising the location of the minimum value as the first element of array. This variable point to the minimum value in the array after the loop.
+    
+    for (count = 1; count < setlength; count++)     //Loop through to find the minimum value in the array by comparing and updating the location variable.
+        if (numberset[count] < numberset[location])
+            location = count;
+    
+    s.min = numberset[location];                    //the array member at the location value will be the minimum value.
+    
+    location = 0; // initialising location variable back to 0
+    
+    for (count = 1; count < setlength; count++)     //Loop through to find the maximum value in the array by comparing and updating the location variable.
+       if (numberset[count] > numberset[location])
+           location = count;
+    
+    s.max = numberset[location];                    //the array member at the location value will be the minimum value.
+    
+    return s;
 }
 
 int emailAlertCallCount = 0;
